4087738bc88a9b9ebf3002d8bd1464334ee34ea2
Claudia María Argueta Caneza <91162449+ClaudiaArguetaCaneza@users.noreply.github.com>
2023-05-06 12:57:33 -0600

    menus ac17033

--- app/src/main/java/com/example/g10proyecto01/EventoEspecialMenuActivity.java
+++ app/src/main/java/com/example/g10proyecto01/EventoEspecialMenuActivity.java
@@ -2,13 +2,44 @@ package com.example.g10proyecto01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EventoEspecialMenuActivity extends AppCompatActivity {
+public class EventoEspecialMenuActivity extends ListActivity  {
 
+    String[] activities={"EventoEspecialInsertarActivity","EventoEspecialConsultarActivity","EventoEspecialActualizarActivity", "EventoEspecialEliminarActivity"};
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_evento_especial_menu);
+        String[] menu={ getResources().getString(R.string.insertar_registro),
+                getResources().getString(R.string.consultar_registro),
+                getResources().getString(R.string.actualizar_registro),
+                getResources().getString(R.string.eliminar_registro)};
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu));
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.parseColor("#B67291"));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+
+        String nombreValue=activities[position];
+
+        l.getChildAt(position).setBackgroundColor(Color.parseColor("#9a5071"));
+
+        try{
+            Class<?> clase=Class.forName("com.example.g10proyecto01." + nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/g10proyecto01/LocalAdministrado.java
+++ app/src/main/java/com/example/g10proyecto01/LocalAdministrado.java
@@ -5,11 +5,60 @@ public class LocalAdministrado {
     private int id_local_admin;
     private Local Edificios;
     private Local nombrelocalidad;
-    //private Empleado_UES nombreempleado;
-    //private Empleado_UES apellidoempleado;
+    private EmpleadoUES nombreempleado;
+    private EmpleadoUES apellidoempleado;
 
     public LocalAdministrado(){
     }
     //Constructor
+
+    public LocalAdministrado(int id_local_admin, Local edificios, Local nombrelocalidad, EmpleadoUES nombreempleado, EmpleadoUES apellidoempleado) {
+        this.id_local_admin = id_local_admin;
+        Edificios = edificios;
+        this.nombrelocalidad = nombrelocalidad;
+        this.nombreempleado = nombreempleado;
+        this.apellidoempleado = apellidoempleado;
+    }
+
     // Getter y Setter
+
+    public int getId_local_admin() {
+        return id_local_admin;
+    }
+
+    public void setId_local_admin(int id_local_admin) {
+        this.id_local_admin = id_local_admin;
+    }
+
+    public Local getEdificios() {
+        return Edificios;
+    }
+
+    public void setEdificios(Local edificios) {
+        Edificios = edificios;
+    }
+
+    public Local getNombrelocalidad() {
+        return nombrelocalidad;
+    }
+
+    public void setNombrelocalidad(Local nombrelocalidad) {
+        this.nombrelocalidad = nombrelocalidad;
+    }
+
+    public EmpleadoUES getNombreempleado() {
+        return nombreempleado;
+    }
+
+    public void setNombreempleado(EmpleadoUES nombreempleado) {
+        this.nombreempleado = nombreempleado;
+    }
+
+    public EmpleadoUES getApellidoempleado() {
+        return apellidoempleado;
+    }
+
+    public void setApellidoempleado(EmpleadoUES apellidoempleado) {
+        this.apellidoempleado = apellidoempleado;
+    }
 }
