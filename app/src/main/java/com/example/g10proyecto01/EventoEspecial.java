3be8a3a4d989b2a9f92492a20b03dbeea10db4b4
Claudia María Argueta Caneza <91162449+ClaudiaArguetaCaneza@users.noreply.github.com>
2023-05-05 23:45:58 -0600

    ac17033 Activity

---
+++ app/src/main/java/com/example/g10proyecto01/EventoEspecial.java
@@ -0,0 +1,20 @@
+package com.example.g10proyecto01;
+
+public class EventoEspecial{
+    private int id_evento;
+    private String organizador;
+    private String nombre_evento;
+    private TipoEvento nombre_tipo_evento;
+    private Local Edificio;
+    private Local nombre_localidad;
+    private String fecha_evento;
+    //private Horario horainicio;
+    //private Horario horafinalizacion;
+
+    public EventoEspecial(){
+    }
+
+    //Constructor
+    // Getter y Setter
+
+}
---
+++ app/src/main/java/com/example/g10proyecto01/TipoEventoEliminarActivity.java
@@ -0,0 +1,14 @@
+package com.example.g10proyecto01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipoEventoEliminarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipo_evento_eliminar);
+    }
+}
