3261d5e68099a1effb1c57b2e35d2181a26b0f99
Misael Gómez <gg20031@ues.edu.sv>
2023-05-06 10:30:13 -0600

    Ejemplo de AlertDialog en pantalla eliminar Tipo empleado
    Se agrego un ejemplo para alerta en los botones de eliminar.

--- app/src/main/java/com/example/g10proyecto01/TipoEmpleadoEliminarActivity.java
+++ app/src/main/java/com/example/g10proyecto01/TipoEmpleadoEliminarActivity.java
@@ -1,14 +1,52 @@
 package com.example.g10proyecto01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-public class TipoEmpleadoEliminarActivity extends AppCompatActivity {
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+public class TipoEmpleadoEliminarActivity extends Activity {
+    EditText editTipoEmpleado;
+    //ControlBDCarnet controlhelper;
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_empleado_eliminar);
+        //controlhelper=new ControlBDCarnet (this);
+        editTipoEmpleado=(EditText)findViewById(R.id.editIdEmpleado);
+    }
+
+    public void eliminarTipoEmpleado(View v){
+        AlertDialog.Builder alerta = new AlertDialog.Builder(TipoEmpleadoEliminarActivity.this);
+        alerta.setCancelable(false);
+        alerta.setMessage("¿Desea eliminar este registro?");
+        alerta.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //ACCIONES SI RESPONDE QUE SI A LA ALERTA
+                String regEliminadas;
+                TipoEmpleado tipoEmpleado=new TipoEmpleado();
+                tipoEmpleado.setId_tipo_empleado(editTipoEmpleado.getText().toString());
+                //controlhelper.abrir();
+                //regEliminadas=controlhelper.eliminar(alumno);
+                regEliminadas = "Se elimino registros";
+                //controlhelper.cerrar();
+                Toast.makeText(TipoEmpleadoEliminarActivity.this, regEliminadas, Toast.LENGTH_SHORT).show();
+            }
+        });
+        alerta.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //ACCIONES SI PONE NO
+                dialog.cancel();
+            }
+        });
+        //MUESTRA ALERTA PARA EVENTO ONCLICK DEL BOTON
+        alerta.show();
+
     }
 }
