8f3f954879ed79582d1ae2c534998fd78f41fd0a
Alexander <hs19011@ues.edu.sv>
2023-05-06 12:06:13 -0600

    gracias por hacerme las clases misa :)

---
+++ app/src/main/java/com/example/g10proyecto01/OfertaAcademicaMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.g10proyecto01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfertaAcademicaMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oferta_academica_menu);
+    }
+}
